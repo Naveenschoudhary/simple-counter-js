b7578a8806aeed041dd84ab37e79e0133fa7a72c
naveen s choudhary <naveenschoudhary.work@gmail.com>
2022-06-11 08:25:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// set initial count
+let count = 0;
+
+//select the value and button
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll(".btn");
+
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('increase')){
+            count++
+        }
+        else if(styles.contains('reset')){
+            count=0
+        }
+        if(count>0){
+            value.style.color = "green"
+        }
+        if(count<0){
+            value.style.color = "red"
+        }
+        if(count===0){
+            value.style.color = "blue"
+        }
+        value.textContent =  count;
+    });
+});
